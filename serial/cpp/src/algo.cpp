06bbe945d5fc94b64e05ed57119904870a2b2c5c
angrajales <agrajal7@eafit.edu.co>
2019-03-12 04:05:07 +0100

    Adding cpp

--- serial/cpp/src/algo.cpp
+++ serial/cpp/src/algo.cpp
@@ -2,33 +2,50 @@
 #include <stdint.h>
 #include <math.h>
 
+
+
 typedef int8_t sint;
 typedef int16_t lint;
 
 const int MAX_SIZE_MB = (lint) 16;
 const int MAX_WIDTH = (lint) 1000;
 const int MAX_HEIGHT = (lint) 1000;
+
 char frame1[MAX_WIDTH][MAX_HEIGHT], frame2[MAX_WIDTH][MAX_HEIGHT]; 
+int __result__[MAX_WIDTH][MAX_HEIGHT];
 
 int global_width = -1, global_height = -1;
 
 using namespace std;
+
+
 
 //Taken from https://stackoverflow.com/questions/9296059/read-pixel-value-in-bmp-file
 
 inline unsigned char * readBMP(char* filename){
   int i;
   FILE *f = fopen(filename, "rb");
+  if (f == NULL) perror ("Error opening data"), exit(1);
   unsigned char info[54];
   fread(info, sizeof(unsigned char), 54, f);
   int width = *(int*) &info[18];
   int height = *(int*) &info[22];
-
+  int bpp = *(int*) &info[28];
+  cerr << "BPP = " << bpp << endl;
+  if (bpp != 8){
+    puts("You have to enter a gray scale image");
+    exit(1);
+  }
   global_width = width, global_height = height;
-  int size = width * height;
+  int size = 3 * width * height;
   unsigned char* data = new unsigned char[size];
   fread(data, sizeof(unsigned char), size, f);
   fclose(f);
+  for(i = 0; i < size; i += 3){
+    unsigned char tmp = data[i];
+    data[i] = data[i+2];
+    data[i+2] = tmp;
+  }
   return data;
   
 }
@@ -41,11 +58,14 @@ inline int calculate_cost(lint i, lint j, lint dx, lint dy){
   return cost;
 }
 
-inline void compute_matrix(lint width1, lint height1, lint width2, lint height2){
-  lint min = (1 << 15 - 1);
+
+
+inline void ** compute_matrix(lint width1, lint height1, lint width2, lint height2){
+  int min = (1 << 30 - 1);
   lint min_x, min_y, min_dx, min_dy;
   min_x = min_y = min_dx = min_dy = 0;
   lint i, j, dx, dy;
+  int __temp__[MAX_HRIGH]
   for(i = 0; i < 1 + width1 - MAX_SIZE_MB; ++i){
     for(j = 0; j < 1 + height2 - MAX_SIZE_MB; ++j){
       for(dx = 0; dx < 1 + width2 - MAX_SIZE_MB; ++dx){
@@ -73,18 +93,19 @@ inline void compute_matrix(lint width1, lint height1, lint width2, lint height2)
 
 void * precompute_matrix(unsigned char * _frame1, unsigned char * _frame2){
   int i, j;
+  
   for(i = 0; i < global_width; ++i){
     for(j = 0; j < global_height; ++j){
-      frame1[i][j] = (_frame1[(i * global_width + j)]);
-      frame2[i][j] = (_frame2[(i * global_width + j)]);
+      frame1[i][j] = (_frame1[(int)(i * global_width + j)]);
+      frame2[i][j] = (_frame2[(int)(i * global_width + j)]);
     }
   }
 }
 
-char * filename1 = "data/frame1.bmp";
-char * filename2 = "data/frame2.bmp";
+char * filename1 = "data/lena_gray.bmp";
+char * filename2 = "data/lena_gray_copy.bmp";
 int main(int argc, char ** argv, char ** env){
-  //printf("%s\n", filename);
+  //printf("%s\n", filename1);
   unsigned char * _frame1 = readBMP(filename1);
   unsigned char * _frame2 = readBMP(filename2);
   void * result = precompute_matrix(_frame1, _frame2);
