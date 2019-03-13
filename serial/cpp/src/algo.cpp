62c5a5fa55111cd8237823b9e730059e07a97d1b
Anderson Daniel Grajales Alzate <agrajal7@hdpmaster.dis.eafit.edu.co>
2019-03-12 23:22:35 -0500

    Data

--- serial/cpp/src/algo.cpp
+++ serial/cpp/src/algo.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <stdint.h>
-#include <math.h>
+#include <cmath>
+#include <stdio.h>
+#include <cstdlib>
+#include <string.h>
 
+typedef int sint;
+typedef int lint;
 
-
-typedef int8_t sint;
-typedef int16_t lint;
-
-const int MAX_SIZE_MB = (lint) 16;
-const int MAX_WIDTH = (lint) 1000;
-const int MAX_HEIGHT = (lint) 1000;
+const int MAX_SIZE_MB = (int) 16;
+const int MAX_WIDTH = (int) 1000;
+const int MAX_HEIGHT = (int) 1000;
 
 char frame1[MAX_WIDTH][MAX_HEIGHT], frame2[MAX_WIDTH][MAX_HEIGHT]; 
 int __result__[MAX_WIDTH][MAX_HEIGHT];
@@ -19,10 +20,9 @@ int global_width = -1, global_height = -1;
 using namespace std;
 
 
-
 //Taken from https://stackoverflow.com/questions/9296059/read-pixel-value-in-bmp-file
 
-inline unsigned char * readBMP(char* filename){
+/*inline unsigned char * readBMP(char* filename){
   int i;
   FILE *f = fopen(filename, "rb");
   if (f == NULL) perror ("Error opening data"), exit(1);
@@ -32,7 +32,7 @@ inline unsigned char * readBMP(char* filename){
   int height = *(int*) &info[22];
   int bpp = *(int*) &info[28];
   //cerr << "BPP = " << bpp << endl;
-  if (bpp != 8){
+  if (bpp != 24){
     puts("You have to enter a gray scale image");
     exit(1);
   }
@@ -40,16 +40,40 @@ inline unsigned char * readBMP(char* filename){
   int size = 3 * width * height;
   unsigned char* data = new unsigned char[size];
   fread(data, sizeof(unsigned char), size, f);
-  //cerr << data << endl;
   fclose(f);
   for(i = 0; i < size; i += 3){
     unsigned char tmp = data[i];
     data[i] = data[i+2];
     data[i+2] = tmp;
-  }
+    cerr << data[i + 2] << endl;
+    }
+  return data;  
+}*/
+unsigned char* readBMP(char* filename)
+{
+  int i;
+  FILE* f = fopen(filename, "rb");
+  unsigned char info[54];
+  fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+  // extract image height and width from header
+  int width = *(int*)&info[18];
+  int height = *(int*)&info[22];
+  cerr << "Width = " << width << " , " << "Height = " << height << endl; 
+  global_width = width, global_height = height;
+  int size = width * height;
+  unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
+  fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
+  fclose(f);
+  /*
+  for(i = 0; i < size; i += 3)
+    {
+      //cerr << (data[i] & 0xFF) << endl;
+    }*/
   return data;
-  
 }
+
+
 
 inline int calculate_cost(lint i, lint j, lint dx, lint dy){
   int cost = 0;
@@ -63,21 +87,23 @@ inline int calculate_cost(lint i, lint j, lint dx, lint dy){
 
 inline void compute_matrix(lint width1, lint height1, lint width2, lint height2){
   int min = (1 << 30 - 1);
-  lint min_x, min_y, min_dx, min_dy;
+  int min_x, min_y, min_dx, min_dy;
   min_x = min_y = min_dx = min_dy = 0;
-  lint i, j, dx, dy;
+  int i, j, dx, dy;
+  int cnt = 0;
   for(i = 0; i < 1 + width1 - MAX_SIZE_MB; ++i){
     for(j = 0; j < 1 + height2 - MAX_SIZE_MB; ++j){
       for(dx = 0; dx < 1 + width2 - MAX_SIZE_MB; ++dx){
 	for(dy = 0; dy < 1 + height2 - MAX_SIZE_MB; ++dy){
-	  if( i != dx && j != dy){
+	  //if( i != dx && j != dy){
 	    int cur_cost = calculate_cost(i, j, dx, dy);
 	    if(cur_cost == 0){
+	      cnt += 1;
 	      min = cur_cost;
 	      min_x = i, min_y = j;
 	      min_dx = dx, min_dy = dy;
 	      __result__[i][j] = 0;
-	      return;
+	      break;
 	    }else{
 	      if(cur_cost < min){
 		min = cur_cost;
@@ -86,35 +112,36 @@ inline void compute_matrix(lint width1, lint height1, lint width2, lint height2)
 	      __result__[i][j] = min;
 	    }
 	  }
-	}
+	//}
 	//cerr << __result__[i][j] << endl;
       }
     }
+  }
+  cerr << cnt << endl;
+  if(cnt == global_width * global_height){
+    cerr << "It works" << endl;
   }
 }
 
 
 void * precompute_matrix(unsigned char * _frame1, unsigned char * _frame2){
   int i, j;
-  
+  //bool eq = true;
   for(i = 0; i < global_width; ++i){
     for(j = 0; j < global_height; ++j){
       frame1[i][j] = (_frame1[(int)(i * global_width + j)]);
       frame2[i][j] = (_frame2[(int)(i * global_width + j)]);
-    }
+      //      if(frame1[i][j] != frame2[i][j]) eq = false;
+   }
   }
-  //cerr << "Here" << endl;
+  //cerr << (eq ? "Yes": "No") << endl;
 }
 
-char * filename1 = "data/lena_gray.bmp";
-char * filename2 = "data/lena_gray.bmp";
+char * filename1 = (char *) "data/w3c_home_gray.bmp";
+char * filename2 = (char * ) "data/w3c_home_gray.bmp";
 int main(int argc, char ** argv, char ** env){
-  //printf("%s\n", filename1);
   unsigned char * _frame1 = readBMP(filename1);
   unsigned char * _frame2 = readBMP(filename2);
-  for (int i = 0; i < sizeof(_frame1)/sizeof(_frame1[0]); ++i){
-    cerr << _frame1[i] << " " << _frame2[i] << endl;
-  }
   void * result = precompute_matrix(_frame1, _frame2);
   compute_matrix(global_width, global_height, global_width, global_height);
   // Now __result__(i, j) is the min(i+dx, j + dx), i=1,2,3,...; j = 1, 2, 3, ...; dx, dx in {16}
